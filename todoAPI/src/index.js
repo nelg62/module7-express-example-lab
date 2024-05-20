5674b2cb5618185e68c53b2a2713565d43e67d9a
nelg62 <glen.harding.nz@gmail.com>
2024-05-20 13:56:31 +1200

    kind of works edit does not actually update inf state

--- todoAPI/src/index.js
+++ todoAPI/src/index.js
@@ -4,7 +4,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 3000;
-const todos = [];
+const todos = [
+  { id: 1, title: "hello" },
+  { id: 2, title: "dijsdg" },
+  { id: 3, title: "dijdf342sdg" },
+];
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -13,12 +17,15 @@ app.get("/todos", (req, res) => {
   res.json(todos);
 });
 
-app.put("/todos", (req, res) => {
-  const tempTodo = action.payload;
-  const foundIndex = [...state].indexOf((todo) => todo.id === tempTodo.id);
-  const copyState = [...state];
-  const spliceResponse = copyState.splice(foundIndex, 1, tempTodo);
-  res.send(spliceResponse);
+app.put("/updatetodos", (req, res) => {
+  // console.log("req.bog", req.body);
+  const tempTodo = req.body;
+  const foundIndex = todos.indexOf((todo) => todo.id === tempTodo.id);
+  todos[foundIndex] = tempTodo;
+
+  // const copyState = [...state];
+  // res.send(copyState.splice(foundIndex, 1, tempTodo));
+  res.json(tempTodo);
 });
 
 app.post("/todos", (req, res) => {
@@ -30,6 +37,18 @@ app.post("/todos", (req, res) => {
   const tempTodo = { ...todo, id: index };
   todos.push(tempTodo);
   res.json(tempTodo);
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const tempTodoId = parseInt(req.params.id);
+  const foundIndex = todos.findIndex((todo) => todo.id === tempTodoId);
+  if (foundIndex > -1) {
+    const deletedTodos = todos.splice(foundIndex, 1);
+    console.log(deletedTodos);
+    res.json(deletedTodos);
+  } else {
+    res.status(404).json({ message: "Todo with id not found " });
+  }
 });
 
 app.listen(port, () => {
